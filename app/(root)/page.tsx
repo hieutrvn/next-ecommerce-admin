8f00d0d72563e6d3c268641ff5052b55529d1df3
Hieu Tran <tranhieuhalai@gmail.com>
2023-07-01 12:16:54 +0700

    Clerk Authentication (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
 
-export default function Home() {
+export default function SetupPage() {
     return (
         <div className=' p-4'>
-            <Button>Click Me</Button>
+            <UserButton afterSignOutUrl='/' />
         </div >
     )
 }
